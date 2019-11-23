ba219fefcd1c8707611f7fa44a63ee29b470285c
mike.rudd <mike@michaelrudd.org.uk>
2019-11-23 18:12:39 +0000

    Fleshed out all content. More detail required on some secitons and styling needs to be addressed.

--- web/js/smooth-scroll.js
+++ web/js/smooth-scroll.js
@@ -1,5 +1,5 @@
 $('a').click(function(){
-    $('html, body').animate({
+    $('html, body, footer').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 2000);
     return false;
